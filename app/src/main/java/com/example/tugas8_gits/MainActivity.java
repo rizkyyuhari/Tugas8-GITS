ac35e4eb8b209eeeca6632570e33dac79f8c2a3a
Rizky <yuharimuhammadrizky@gmail.com>
2021-09-10 20:36:14 +0700

    tambah libary push down animation click

--- app/src/main/java/com/example/tugas8_gits/MainActivity.java
+++ app/src/main/java/com/example/tugas8_gits/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.tugas8_gits;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+import com.thekhaeng.pushdownanim.PushDownAnim;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        PushDownAnim.setPushDownAnimTo(btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Push Down !!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        initView();
     }
 
 
+    private void initView() {
+        btn = findViewById(R.id.btn);
+    }
 }
